30a9bba1c8a892b6c60b560ab4619143207ba7cd
Ramdan Syaripudin <macbook@Macbook.local>
2025-01-25 17:20:10 +0700

    data validation implemented

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
 const NotesService = require('./services/inMemory/NotesService');
+const NotesValidator = require('./validator/notes');
+const { valid } = require('joi');
 
 const init = async () => {
   const notesService = new NotesService();
@@ -18,6 +20,7 @@ const init = async () => {
     plugin: notes,
     options: {
       service: notesService,
+      validator: NotesValidator,
     },
   });
 
