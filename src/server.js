7600c6d625786aca266c4b35649448878dac8e36
Ramdan Syaripudin <macbook@Macbook.local>
2025-03-02 13:04:56 +0700

    collaboration implemented

--- src/server.js
+++ src/server.js
@@ -20,8 +20,14 @@ const TokenManager = require('./tokenize/tokenManager');
 const AuthenticationsValidator = require('./validator/authentications');
 const AuthenticationsService = require('./services/postgres/AuthenticationsService');
 
+// collaborations
+const collaborations = require('./api/collaborations');
+const CollaborationsService = require('./services/postgres/CollaborationsService');
+const CollaborationsValidator = require('./validator/collaborations');
+
 const init = async () => {
-  const notesService = new NotesService();
+  const collaborationsService = new CollaborationsService();
+  const notesService = new NotesService(collaborationsService);
   const usersService = new UsersService();
   const authenticationsService = new AuthenticationsService();
 
@@ -88,6 +94,14 @@ const init = async () => {
         usersService,
         tokenManager: TokenManager,
         validator: AuthenticationsValidator,
+      },
+    },
+    {
+      plugin: collaborations,
+      options: {
+        collaborationsService,
+        notesService,
+        validator: CollaborationsValidator,
       },
     },
   ]);
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,10 @@
-const mapDBToModel = ({ id, title, body, tags, created_at, updated_at }) => ({
+const mapDBToModel = ({ id, title, body, tags, created_at, updated_at, username }) => ({
   id,
   title,
   body,
   tags,
   createdAt: created_at,
   updatedAt: updated_at,
+  username,
 });
 module.exports = { mapDBToModel };
